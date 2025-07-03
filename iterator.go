bae3dccf03320f72d3e9573a68b066f0d5e2aaa4
pchchv <ipchchv@gmail.com>
2025-07-03 20:30:35 +0300

    Add PeekableIterator type for non-advancing element access

--- iterator.go
+++ iterator.go
@@ -10,3 +10,12 @@ type Iterator[T any] interface {
 	// Returns an Option with value of None when iteration has finished.
 	Next() optionext.Option[T]
 }
+
+// PeekableIterator is an interface representing something that
+// iterates using the Next method and ability to `Peek` the
+// next element value without advancing the `Iterator`.
+type PeekableIterator[T any] interface {
+	Iterator[T]
+	// Peek returns the `Next` element from the `Iterator` without advancing it.
+	Peek() optionext.Option[T]
+}
