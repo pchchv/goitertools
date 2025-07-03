198e28d24018497a4e9223e75a36b60e0f333812
pchchv <ipchchv@gmail.com>
2025-07-03 20:29:23 +0300

    Add Iterator type for iteration support

--- iterator.go
+++ iterator.go
@@ -1,2 +1,12 @@
 package goitertools
 
+import "github.com/pchchv/express/optionext"
+
+// Iterator is an interface that represents
+// something performing an iteration using the Next method.
+type Iterator[T any] interface {
+	// Next advances the iterator and returns the next value.
+	//
+	// Returns an Option with value of None when iteration has finished.
+	Next() optionext.Option[T]
+}
