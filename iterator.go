6e7300405fe58fe803e6baae3a0a8b1cdffce8e6
pchchv <ipchchv@gmail.com>
2025-07-03 20:35:31 +0300

    Add forEach method to Iterate type for early cancellable iteration

--- iterator.go
+++ iterator.go
@@ -1,6 +1,13 @@
 package goitertools
 
-import "github.com/pchchv/express/optionext"
+import (
+	"runtime"
+	"sync"
+
+	"github.com/pchchv/express/optionext"
+)
+
+var numCPU = runtime.NumCPU()
 
 // Iterator is an interface that represents
 // something performing an iteration using the Next method.
@@ -28,4 +35,52 @@ type Iterate[T any, I Iterator[T], MAP any] struct {
 // Next returns the new iterator value.
 func (i Iterate[T, I, MAP]) Next() optionext.Option[T] {
 	return i.iterator.Next()
+}
+
+// forEach is an early cancellable form of `ForEach`.
+func (i Iterate[T, I, MAP]) forEach(parallel bool, fn func(T) (stop bool)) {
+	if parallel {
+		var stopOnce sync.Once
+		stopEarly := make(chan struct{})
+		in := make(chan optionext.Option[T])
+		wg := new(sync.WaitGroup)
+		for j := 0; j < numCPU; j++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+			FOR:
+				for {
+					select {
+					case <-stopEarly:
+						break FOR
+					case v := <-in:
+						if v.IsNone() || fn(v.Unwrap()) {
+							stopOnce.Do(func() {
+								close(stopEarly)
+							})
+							break FOR
+						}
+
+					}
+				}
+			}()
+		}
+	FOR:
+		for {
+			select {
+			case <-stopEarly:
+				break FOR
+			case in <- i.iterator.Next():
+			}
+		}
+		close(in)
+		wg.Wait()
+	} else {
+		for {
+			v := i.iterator.Next()
+			if v.IsNone() || fn(v.Unwrap()) {
+				break
+			}
+		}
+	}
 }
