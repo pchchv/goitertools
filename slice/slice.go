a7c5cda87647c89873477cc312dd9d406f3859af
pchchv <ipchchv@gmail.com>
2025-07-04 08:24:45 +0300

    Add Sort and SortStable functions for slice sorting

--- slice/slice.go
+++ slice/slice.go
@@ -1,1 +1,23 @@
 package slice
+
+import "sort"
+
+// Sort sorts the sliceWrapper x given the provided less function.
+//
+// The sort is not guaranteed to be stable:
+// equal elements may be reversed from their original order.
+//
+// For a stable sort, use SortStable.
+func Sort[T any](slice []T, less func(i T, j T) bool) {
+	sort.Slice(slice, func(j, k int) bool {
+		return less(slice[j], slice[k])
+	})
+}
+
+// SortStable sorts the sliceWrapper x using the provided less function,
+// keeping equal elements in their original order.
+func SortStable[T any](slice []T, less func(i T, j T) bool) {
+	sort.SliceStable(slice, func(j, k int) bool {
+		return less(slice[j], slice[k])
+	})
+}
