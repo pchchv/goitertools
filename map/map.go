fbff6691594829ba0b39d496560f2443d467627c
pchchv <ipchchv@gmail.com>
2025-07-07 19:53:33 +0300

    Implement Map function for mapping a map[K]V to U

--- map/map.go
+++ map/map.go
@@ -1,1 +1,11 @@
 package mapext
+
+// Map allows mapping of a map[K]V -> U.
+func Map[K comparable, V any, U any](m map[K]V, init U, fn func(accum U, key K, value V) U) U {
+	accum := init
+	for k, v := range m {
+		accum = fn(accum, k, v)
+	}
+
+	return accum
+}
