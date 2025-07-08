5dc7847223b73f06729391c31a5ccc7bc3f529a3
pchchv <ipchchv@gmail.com>
2025-07-08 21:27:57 +0300

    Implement Next and Iter methods for mapper type to enhance transformation functionality

--- mapper.go
+++ mapper.go
@@ -1,5 +1,7 @@
 package goitertools
 
+import "github.com/pchchv/express/optionext"
+
 // MapFn represents the mapWrapper transformation function.
 type MapFn[T, MAP any] func(v T) MAP
 
@@ -7,4 +9,19 @@ type MapFn[T, MAP any] func(v T) MAP
 type mapper[T any, I Iterator[T], MAP any] struct {
 	iterator I
 	fn       MapFn[T, MAP]
+}
+
+// Next returns the next transformed element or None if at the end of the iterator.
+func (i mapper[T, I, MAP]) Next() optionext.Option[MAP] {
+	v := i.iterator.Next()
+	if v.IsNone() {
+		return optionext.None[MAP]()
+	}
+
+	return optionext.Some(i.fn(v.Unwrap()))
+}
+
+// Iter is a convenience function that converts the map iterator into an `*Iterate[T]`.
+func (i mapper[T, I, MAP]) Iter() Iterate[MAP, Iterator[MAP], struct{}] {
+	return Iter[MAP, Iterator[MAP]](i)
 }
