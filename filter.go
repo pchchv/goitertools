3679b4085e61eec1d7c8330da32e8adb89eb3e4a
pchchv <ipchchv@gmail.com>
2025-07-08 21:17:02 +0300

    Define filterIterator type for filtering Iterator[T]

--- filter.go
+++ filter.go
@@ -2,3 +2,9 @@ package goitertools
 
 // FilterFn represents the `filterIterator` function.
 type FilterFn[T any] func(v T) bool
+
+// filterIterator allows filtering of an `Iterator[T]`.
+type filterIterator[T any, I Iterator[T], MAP any] struct {
+	iterator I
+	fn       FilterFn[T]
+}
