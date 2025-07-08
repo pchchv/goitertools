61b6c04d1ebed0855dfdc947e18e315c760885a5
pchchv <ipchchv@gmail.com>
2025-07-08 21:17:40 +0300

    Implement Next and Iter methods for filterIterator

--- filter.go
+++ filter.go
@@ -1,5 +1,7 @@
 package goitertools
 
+import "github.com/pchchv/express/optionext"
+
 // FilterFn represents the `filterIterator` function.
 type FilterFn[T any] func(v T) bool
 
@@ -7,4 +9,20 @@ type FilterFn[T any] func(v T) bool
 type filterIterator[T any, I Iterator[T], MAP any] struct {
 	iterator I
 	fn       FilterFn[T]
+}
+
+// Next yields the next value from the iterator that passed the filter function.
+func (i *filterIterator[T, I, MAP]) Next() optionext.Option[T] {
+	for {
+		v := i.iterator.Next()
+		if v.IsNone() || !i.fn(v.Unwrap()) {
+			return v
+		}
+	}
+}
+
+// Iter is a convenience function that converts the
+// `filterIterator` iterator into an `Iterate[T]`.
+func (i *filterIterator[T, I, MAP]) Iter() Iterate[T, Iterator[T], MAP] {
+	return IterMap[T, Iterator[T], MAP](i)
 }
