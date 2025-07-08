932ed2929523e6063794df1380ed8fbe17a7d390
pchchv <ipchchv@gmail.com>
2025-07-08 21:26:30 +0300

    Add mapper type for transforming elements in the iterator

--- mapper.go
+++ mapper.go
@@ -2,3 +2,9 @@ package goitertools
 
 // MapFn represents the mapWrapper transformation function.
 type MapFn[T, MAP any] func(v T) MAP
+
+// mapWrapper is used to transform elements from one type to another.
+type mapper[T any, I Iterator[T], MAP any] struct {
+	iterator I
+	fn       MapFn[T, MAP]
+}
