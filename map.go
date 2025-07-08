1b3288c00b9fc30f1e83edad792672cc679321b9
pchchv <ipchchv@gmail.com>
2025-07-08 21:07:44 +0300

    Add mapWrapper type for transforming elements in goitertools package

--- map.go
+++ map.go
@@ -5,3 +5,8 @@ type Entry[K comparable, V any] struct {
 	Key   K
 	Value V
 }
+
+// mapWrapper is used to transform elements from one type to another.
+type mapWrapper[K comparable, V, MAP any] struct {
+	m map[K]V
+}
