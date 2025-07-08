64c0cb31f8b5a1617918c20d8e92acb8a21a3313
pchchv <ipchchv@gmail.com>
2025-07-08 21:12:25 +0300

    Add benchmark for Retain method in mapWrapper type

--- map_test.go
+++ map_test.go
@@ -1,5 +1,19 @@
 package goitertools
 
+import (
+	"testing"
+
+	mapext "github.com/pchchv/goitertools/map"
+)
+
+func BenchmarkRetainMap_Retain(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		mapext.Retain(makeMap(), func(_ string, value int) (retain bool) {
+			return value == 3
+		})
+	}
+}
+
 func makeMap() map[string]int {
 	return map[string]int{
 		"1": 1,
