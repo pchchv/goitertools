f6fbcc4cceca84edeb63467e8295b11fc88e0f1c
pchchv <ipchchv@gmail.com>
2025-07-09 20:23:23 +0300

    Implement Next and Iter methods for sliceWrapper type

--- slice.go
+++ slice.go
@@ -3,3 +3,18 @@ package goitertools
 type sliceWrapper[T, MAP any] struct {
 	slice []T
 }
+
+// Len returns the length of the underlying sliceWrapper.
+func (i sliceWrapper[T, MAP]) Len() int {
+	return len(i.slice)
+}
+
+func (i *sliceWrapper[T, MAP]) Next() optionext.Option[T] {
+	if len(i.slice) == 0 {
+		return optionext.None[T]()
+	}
+
+	v := i.slice[0]
+	i.slice = i.slice[1:]
+	return optionext.Some(v)
+}
