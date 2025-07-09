d0c1da8e990e382a664a1fdef7f19c18aae21b82
pchchv <ipchchv@gmail.com>
2025-07-09 20:43:35 +0300

    Implment Next method for fakeIterator type

--- fake.go
+++ fake.go
@@ -1,5 +1,16 @@
 package goitertools
 
+import "github.com/pchchv/express/optionext"
+
 type fakeIterator struct {
 	max int
+}
+
+func (f *fakeIterator) Next() optionext.Option[int] {
+	f.max--
+	if f.max < 0 {
+		return optionext.None[int]()
+	}
+
+	return optionext.Some(f.max)
 }
