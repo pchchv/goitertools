b560eab43d3725fb8da69ceeb57615763cd469fd
pchchv <ipchchv@gmail.com>
2025-07-09 20:36:38 +0300

    Add tests fo mapWrapper type

--- map_test.go
+++ map_test.go
@@ -3,8 +3,52 @@ package goitertools
 import (
 	"testing"
 
+	"github.com/pchchv/express/optionext"
+	. "github.com/pchchv/go-assert"
 	mapext "github.com/pchchv/goitertools/map"
 )
+
+func TestMap(t *testing.T) {
+	// Test Misc
+	iter := WrapMap(makeMap())
+	Equal(t, iter.Len(), 5)
+
+	// Test Next
+	iter = WrapMap(makeMap())
+	Equal(t, iter.Next().IsSome(), true)
+	Equal(t, iter.Next().IsSome(), true)
+	Equal(t, iter.Next().IsSome(), true)
+	Equal(t, iter.Next().IsSome(), true)
+	Equal(t, iter.Next().IsSome(), true)
+	Equal(t, iter.Next().IsSome(), false)
+
+	// Test Retain
+	iter = WrapMap(makeMap()).Retain(func(key string, value int) bool {
+		return value == 3
+	})
+	Equal(t, iter.Next(), optionext.Some(Entry[string, int]{Key: "3", Value: 3}))
+	Equal(t, iter.Next(), optionext.None[Entry[string, int]]())
+
+	// Test Iter Filter
+	iter2 := WrapMap(makeMap()).Iter().Filter(func(v Entry[string, int]) bool {
+		return v.Value != 3
+	})
+	Equal(t, iter2.Next(), optionext.Some(Entry[string, int]{Key: "3", Value: 3}))
+	Equal(t, iter2.Next(), optionext.None[Entry[string, int]]())
+
+	// Test Iter Map
+	iterMap := WrapMapWithMap[string, int, int](makeMap()).Iter().Map(func(v Entry[string, int]) int {
+		return v.Value
+	}).Iter().CollectIter().Sort(func(i int, j int) bool {
+		return i < j
+	})
+	Equal(t, iterMap.Next(), optionext.Some(1))
+	Equal(t, iterMap.Next(), optionext.Some(2))
+	Equal(t, iterMap.Next(), optionext.Some(3))
+	Equal(t, iterMap.Next(), optionext.Some(4))
+	Equal(t, iterMap.Next(), optionext.Some(5))
+	Equal(t, iterMap.Next(), optionext.None[int]())
+}
 
 func BenchmarkRetainMap_Retain(b *testing.B) {
 	for i := 0; i < b.N; i++ {
