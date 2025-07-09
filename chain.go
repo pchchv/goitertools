6f9d53eea0d108cdb1423920df08bb2576361dea
pchchv <ipchchv@gmail.com>
2025-07-09 20:41:09 +0300

    Implement ChainWithMap function to create a new chainIterator with potential Map operation

--- chain.go
+++ chain.go
@@ -28,3 +28,13 @@ func (i *chainIterator[T, FI, SI, MAP]) Next() optionext.Option[T] {
 func (i *chainIterator[T, FI, SI, MAP]) Iter() Iterate[T, Iterator[T], MAP] {
 	return IterMap[T, Iterator[T], MAP](i)
 }
+
+// ChainWithMap creates a new `chainIterator[T]` for
+// use and parameter to specify a Map type for
+// the `Iterate.Map` helper function.
+func ChainWithMap[T any, FI Iterator[T], SI Iterator[T], MAP any](first FI, second SI) *chainIterator[T, FI, SI, MAP] {
+	return &chainIterator[T, FI, SI, MAP]{
+		current: first,
+		next:    second,
+	}
+}
