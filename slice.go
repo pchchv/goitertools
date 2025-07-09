6c4b48ffca286b862d06e2d0ff0c59e2222b519a
pchchv <ipchchv@gmail.com>
2025-07-09 20:25:06 +0300

    Implement Map and Slice methods for sliceWrapper type

--- slice.go
+++ slice.go
@@ -1,6 +1,9 @@
 package goitertools
 
-import "github.com/pchchv/express/optionext"
+import (
+	"github.com/pchchv/express/optionext"
+	"github.com/pchchv/goitertools/slice"
+)
 
 type sliceWrapper[T, MAP any] struct {
 	slice []T
@@ -29,4 +32,14 @@ func (i sliceWrapper[T, MAP]) IntoIter() *sliceWrapper[T, MAP] {
 // Iter is a convenience function that converts the sliceWrapper iterator into an `*Iterate[T]`.
 func (i sliceWrapper[T, MAP]) Iter() Iterate[T, *sliceWrapper[T, MAP], MAP] {
 	return IterMap[T, *sliceWrapper[T, MAP], MAP](i.IntoIter())
+}
+
+// Map maps a slice of []T -> MAP using the map function.
+func (i sliceWrapper[T, MAP]) Map(init MAP, fn func(accum MAP, v T) MAP) MAP {
+	return slice.Map[T, MAP](i.slice, init, fn)
+}
+
+// Slice returns the underlying sliceWrapper wrapped by the *sliceWrapper.
+func (i sliceWrapper[T, MAP]) Slice() []T {
+	return i.slice
 }
