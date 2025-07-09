a1c0dc82008041337875046a96d232af47f25625
pchchv <ipchchv@gmail.com>
2025-07-09 20:23:57 +0300

    Implement IntoIter and Iter methods for sliceWrapper type

--- slice.go
+++ slice.go
@@ -1,5 +1,7 @@
 package goitertools
 
+import "github.com/pchchv/express/optionext"
+
 type sliceWrapper[T, MAP any] struct {
 	slice []T
 }
@@ -17,4 +19,14 @@ func (i *sliceWrapper[T, MAP]) Next() optionext.Option[T] {
 	v := i.slice[0]
 	i.slice = i.slice[1:]
 	return optionext.Some(v)
+}
+
+// IntoIter turns the slice wrapper into an `Iterator[T]`
+func (i sliceWrapper[T, MAP]) IntoIter() *sliceWrapper[T, MAP] {
+	return &i
+}
+
+// Iter is a convenience function that converts the sliceWrapper iterator into an `*Iterate[T]`.
+func (i sliceWrapper[T, MAP]) Iter() Iterate[T, *sliceWrapper[T, MAP], MAP] {
+	return IterMap[T, *sliceWrapper[T, MAP], MAP](i.IntoIter())
 }
