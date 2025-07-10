3f2f68d3f85731913e34220c1958db6797dd8edf
pchchv <ipchchv@gmail.com>
2025-07-10 20:17:08 +0300

    Add takeWhileIterator type

--- take_while.go
+++ take_while.go
@@ -2,3 +2,11 @@ package goitertools
 
 // TakeWhileFn represents the `takeWhileIterator[T]` function.
 type TakeWhileFn[T any] func(v T) bool
+
+// takeWhileIterator is an iterator that iterates over elements until
+// the function return false or end of the iterator
+// (whichever happens first).
+type takeWhileIterator[T any, I Iterator[T], MAP any] struct {
+	iterator Iterator[T]
+	fn       TakeWhileFn[T]
+}
