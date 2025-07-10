9a9059127d4a81da7a6b432923e5134a21c5dc3b
pchchv <ipchchv@gmail.com>
2025-07-10 20:14:42 +0300

    Implement Iter and Next methods for takeIterator type

--- take.go
+++ take.go
@@ -5,3 +5,17 @@ type takeIterator[T any, I Iterator[T], MAP any] struct {
 	iterator I
 	limit    int
 }
+
+// Next returns the next element until n is reached or end of the iterator.
+func (i *takeIterator[T, I, MAP]) Next() optionext.Option[T] {
+	if i.limit <= 0 {
+		return optionext.None[T]()
+	}
+	i.limit--
+	return i.iterator.Next()
+}
+
+// Iter is a convenience function that converts the `takeIterator` iterator into an `*Iterate[T]`.
+func (i *takeIterator[T, I, MAP]) Iter() Iterate[T, Iterator[T], MAP] {
+	return IterMap[T, Iterator[T], MAP](i)
+}
