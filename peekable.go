4e66c33d510ef39af29d3c6d4da55f9f47e9e78a
pchchv <ipchchv@gmail.com>
2025-07-15 21:21:00 +0300

    Implement Next and Peek methods for peekableIterator

--- peekable.go
+++ peekable.go
@@ -7,3 +7,26 @@ type peekableIterator[T any, I Iterator[T]] struct {
 	iterator I
 	prev     optionext.Option[T]
 }
+
+// Next advances the iterator and returns the next value.
+//
+// Returns an Option with value of None when iteration has finished.
+func (i *peekableIterator[T, I]) Next() optionext.Option[T] {
+	if i.prev.IsSome() {
+		prev := i.prev
+		i.prev = optionext.None[T]()
+		return prev
+	}
+
+	return i.iterator.Next()
+}
+
+// Peek returns the next value without advancing the iterator.
+func (i *peekableIterator[T, I]) Peek() optionext.Option[T] {
+	if i.prev.IsSome() {
+		return i.prev
+	}
+
+	i.prev = i.iterator.Next()
+	return i.prev
+}
